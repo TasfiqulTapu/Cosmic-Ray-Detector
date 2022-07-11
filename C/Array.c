6bb27e68dce0037f08f7a021bbec0d22db9cf141
Tapu <66732331+TasfiqulTapu@users.noreply.github.com>
2022-07-11 20:08:34 +0600

    📄 detector in C using Arrays

---
+++ C/Array.c
@@ -0,0 +1,10 @@
+// cosmic ray detector by Tasfiqul Tapu
+#include <stdio.h>
+int main(){
+    int ARRAY[100] = {0};
+    while(1){
+        for(int i=0;i<100;i++){
+!(ARRAY[i])!=0?:printf("Cosmic Rays detected");
+        };
+    };
+};
